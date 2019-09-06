2af7b2e49f8855c257f55ce5fa3ffde6b397f0df
unknown <daniel_fmpc@hotmail.com.br>
2019-09-05 21:38:53 -0300

    Salvar, recuperar e remover

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -7,10 +8,30 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController _controller = TextEditingController();
+  String textoSalvo = "nada Salvo";
+  _salvar() async {
+    String valorDigitado = _controller.text;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("nome", valorDigitado);
+    
+  }
+  _recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      textoSalvo = prefs.getString("nome") ?? "sem valor";
+    });
+  }
+  _remover() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove("nome");
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: Text("Manipulação de dados"),
       ),
       body: Container(
@@ -18,22 +39,30 @@ class _HomeState extends State<Home> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text("Texto"),
+            Text(textoSalvo),
             TextField(
               keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Digite algo",
+              ),
               controller: _controller,              
             ),
             Row(
               children: <Widget>[
                 RaisedButton(
-                  onPressed: null,
+                  onPressed: _salvar(),
                   child: Text("Salvar", style: TextStyle(color: Colors.white),),
+                  color: Colors.green,            
+                ),
+                RaisedButton(
+                  onPressed: _recuperar(),
+                  child: Text("ler", style: TextStyle(color: Colors.white),),
                   color: Colors.blue,            
                 ),
                 RaisedButton(
-                  onPressed: null,
-                  child: Text("ler", style: TextStyle(color: Colors.white),),
-                  color: Colors.blue,            
+                  onPressed: _remover(),
+                  child: Text("remover", style: TextStyle(color: Colors.white),),
+                  color: Colors.red,            
                 ),
               ],
             )          
